8602a4c0df72f46d1baf286dd4b8bd58b7a701b6
cristianperca <cristianperca@yahoo.com>
2020-03-22 14:30:22 +0100

    Complete gatsby site

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import Head from "../components/head"
+
 
 const AboutPage = () => {
   return (
     <div>
       <Layout>
+        <Head title="About" />
         <h1>About me.</h1>
         <p>My name is Cristian and I am 43 years</p>
         <p>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import Head from "../components/head"
 
 const ContactPage = () => {
   return (
     <div>
       <Layout>
+        <Head title="Contact" />
         <h1>Contact page</h1>
         <p>
           You can reach me here{" "}
